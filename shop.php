3732fbe4086409add48edf54bbc9630bc644c899
Marco-Martins-97 <marco.agm97+github@gmail.com>
2025-09-20 09:31:54 +0100

    Shop - style- cart

--- shop.php
+++ shop.php
@@ -59,8 +59,96 @@
             </nav>
         </header>
         <main>
-            <div class="shopping-cart"></div>
+            <div class="cart-btn-container"><i class="fa fa-shopping-cart"></i></div>
+            <div class="shopping-cart">
+                <div class="cart-header">
+                    <h2>Carrinho de Compras</h2>
+                    <span id='close-shopping-cart'>&times;</span>
+                </div>
+                <div class="cart-content">
+                    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sequi alias, nostrum optio, placeat minima doloribus quia odit voluptates adipisci consequuntur suscipit et velit pariatur ut quam ducimus molestiae maiores dolore corporis? Accusantium nesciunt eos vero provident porro ad facilis debitis, fuga, quibusdam rerum delectus incidunt veritatis magni est pariatur voluptatum sit vel ut nobis! Hic, maiores suscipit. Corrupti officiis quam excepturi voluptatibus laborum modi dolorem, nisi pariatur? Numquam aliquam assumenda suscipit eveniet? Non amet excepturi ut natus illum, tenetur quo ipsam laudantium quam, minima ex? Perspiciatis quod esse omnis, eveniet natus molestiae maxime consectetur quis quasi dolore placeat voluptate odit nobis sapiente incidunt recusandae sit ab maiores sed tenetur iusto neque. Delectus molestiae atque consequatur praesentium nam expedita aut corrupti distinctio deserunt vel. Voluptas atque eveniet deleniti perspiciatis explicabo tempore in a debitis earum, molestiae doloremque praesentium pariatur officia, ab ullam repellat illum possimus architecto illo commodi temporibus cumque! Fugiat dolor explicabo ullam quo non quos facere! Quam quas et nesciunt beatae accusamus soluta quasi fugit, mollitia ducimus placeat eveniet at cupiditate consectetur ad sed? Eum quasi quam, quidem recusandae autem iusto saepe blanditiis quo ducimus odio accusantium minus perferendis maiores id, quisquam repudiandae ex? Temporibus pariatur at dolores saepe, laboriosam maxime accusantium quia nam tempore tempora accusamus repudiandae impedit harum similique distinctio praesentium animi, et quo voluptate quae. Aliquid sapiente nihil amet excepturi? Velit odio cum nemo sit aut tempore, quod quis incidunt dolor, facilis dicta nihil odit totam perferendis unde repudiandae, id sunt quae reprehenderit perspiciatis? Quis nesciunt, fugiat ipsum corrupti totam iusto consequatur voluptates accusamus, quibusdam magnam facere reprehenderit consectetur unde veritatis. Ipsa, voluptatum nobis. Hic suscipit sapiente laudantium enim consequatur minus quos accusamus tempora sit possimus, reiciendis corporis est. Similique suscipit cum, repellat omnis repudiandae velit distinctio autem quibusdam praesentium! Ullam totam nostrum similique enim.</P>
+                </div>
+                <button>Comprar</button>
+            </div>
             <div class="shop-content">
+                <div class="product-container">
+                        <img src="imgs/products/notebook.jpg" alt="">
+                        <div class="description-container">
+                            <div class="description">
+                                <h3>Nome do produto 1</h3>
+                                <p>1,99 €</p>
+                            </div>
+                            <div class="btns-container">
+                                <button>Comprar</button>
+                                <button>Adicionar Carrinho</button>
+                            </div>
+                        </div>
+                </div>
+                <div class="product-container">
+                        <img src="imgs/products/notebook.jpg" alt="">
+                        <div class="description-container">
+                            <div class="description">
+                                <h3>Nome do produto 1</h3>
+                                <p>1,99 €</p>
+                            </div>
+                            <div class="btns-container">
+                                <button>Comprar</button>
+                                <button>Adicionar Carrinho</button>
+                            </div>
+                        </div>
+                </div>
+                <div class="product-container">
+                        <img src="imgs/products/notebook.jpg" alt="">
+                        <div class="description-container">
+                            <div class="description">
+                                <h3>Nome do produto 1</h3>
+                                <p>1,99 €</p>
+                            </div>
+                            <div class="btns-container">
+                                <button>Comprar</button>
+                                <button>Adicionar Carrinho</button>
+                            </div>
+                        </div>
+                </div>
+                <div class="product-container">
+                        <img src="imgs/products/notebook.jpg" alt="">
+                        <div class="description-container">
+                            <div class="description">
+                                <h3>Nome do produto 1</h3>
+                                <p>1,99 €</p>
+                            </div>
+                            <div class="btns-container">
+                                <button>Comprar</button>
+                                <button>Adicionar Carrinho</button>
+                            </div>
+                        </div>
+                </div>
+                <div class="product-container">
+                        <img src="imgs/products/notebook.jpg" alt="">
+                        <div class="description-container">
+                            <div class="description">
+                                <h3>Nome do produto 1</h3>
+                                <p>1,99 €</p>
+                            </div>
+                            <div class="btns-container">
+                                <button>Comprar</button>
+                                <button>Adicionar Carrinho</button>
+                            </div>
+                        </div>
+                </div>
+                <div class="product-container">
+                        <img src="imgs/products/notebook.jpg" alt="">
+                        <div class="description-container">
+                            <div class="description">
+                                <h3>Nome do produto 1</h3>
+                                <p>1,99 €</p>
+                            </div>
+                            <div class="btns-container">
+                                <button>Comprar</button>
+                                <button>Adicionar Carrinho</button>
+                            </div>
+                        </div>
+                </div>
                 <div class="product-container">
                         <img src="imgs/products/notebook.jpg" alt="">
                         <div class="description-container">
